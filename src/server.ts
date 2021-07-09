c07b48a91d8f8791a022ce1f2ba1e0d29948e8b6
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-07-08 23:12:12 -0300

    variable missing type notation

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 
 const {NODE_ENV, PORT} = process.env;
-const dev = NODE_ENV === "development" ? " on Dev mode" : "";
+const dev: string = NODE_ENV === "development" ? " on Dev mode" : "";
 
 app.listen(PORT, () => {
   console.log(`Server runing on port ${PORT + dev}`);

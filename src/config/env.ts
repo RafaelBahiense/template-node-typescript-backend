82dd6d4c3d6e7b39c7f417fff744e0a500e7b6c5
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-07-08 23:09:09 -0300

    renamed .env files

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,7 +5,7 @@ export function setEnv(): void {
   const { NODE_ENV } = process.env;
 
   if (NODE_ENV === "development" || NODE_ENV === "test") {
-    const envPath: string = NODE_ENV === "test" ? ".env.test.local" : ".env.local";
+    const envPath: string = NODE_ENV === "test" ? "local.test.env" : "local.dev.env";
     dotenv.config({ path: path.resolve(".", envPath) });
   }
 }

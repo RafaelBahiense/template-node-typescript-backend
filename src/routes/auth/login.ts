3a24acac6153e06983dc69bbf9e24e860477b0d0
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-07-12 12:51:14 -0300

    updated login and register routes

--- src/routes/auth/login.ts
+++ src/routes/auth/login.ts
@@ -1,7 +1,9 @@
 import { Request, Response, Router } from "express";
 
+import login from "../../controllers/auth/login";
+
 const router: Router = Router();
 
-router.post("/login", () => console.log("oi"));
+router.post("/login", (req: Request, res: Response) => login(req, res));
 
 export const Login: Router = router;
--- src/routes/auth/register.ts
+++ src/routes/auth/register.ts
@@ -1,7 +1,9 @@
 import { Request, Response, Router } from "express";
 
+import register from "../../controllers/auth/register";
+
 const router: Router = Router();
 
-router.post("/register", () => console.log("oi"));
+router.post("/register", (req: Request, res: Response) => register(req, res));
 
 export const Register: Router = router;

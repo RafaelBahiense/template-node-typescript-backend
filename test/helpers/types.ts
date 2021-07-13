a667c8a64f6e0c89783d6f69b657d71d472c17fa
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-07-13 03:59:17 -0300

    test types

---
+++ test/helpers/types.ts
@@ -0,0 +1,25 @@
+interface User {
+  email: string;
+  password: string;
+  name?: string;
+}
+
+export class LoginUser implements User {
+  email: string;
+  password: string;
+  constructor({ email = "jest@jest.br", password = "12345678" }) {
+    this.email = email;
+    this.password = password;
+  }
+}
+
+export class RegisterUser implements User {
+  name: string;
+  email: string;
+  password: string;
+  constructor({ name = "Jest" , email = "jest@jest.br", password = "12345678"}) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+}

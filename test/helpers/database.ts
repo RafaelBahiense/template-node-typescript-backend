1c92898e90cef35127dd46609def6811c5ecc250
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-07-13 02:48:49 -0300

    test helper db

---
+++ test/helpers/database.ts
@@ -0,0 +1,16 @@
+import { connectionDB } from "../../src/config/database";
+
+export async function addUser(): Promise<void> {
+  await connectionDB.query(
+    `INSERT INTO users (name, email, hash) 
+        VALUES ('Jest', 'jest@jest.br','$2b$12$4iSzSqv3Cb8KsewVXCfyyu5BZOzEq2CaDh2eTHni75z/ZLvT7suuC')`
+  );
+}
+
+export async function truncateTable(table: string): Promise<void> {
+  await connectionDB.query(`TRUNCATE TABLE ${table} RESTART IDENTITY`);
+}
+
+export function endConnection(): void {
+  connectionDB.end()
+}

40f3e04ff9da6f0ac3774457a354fe56ca781690
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-07-20 21:11:35 -0300

    updated db users & sessions schemas

--- database/scripts/001-create-users.sql
+++ database/scripts/001-create-users.sql
@@ -2,7 +2,7 @@ CREATE TABLE "users" (
 	"id" serial NOT NULL,
 	"name" TEXT NOT NULL,
 	"email" TEXT NOT NULL UNIQUE,
-	"createdAt" timestamp with time zone NOT NULL,
+	"createdAt" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	"hash" TEXT NOT NULL,
 	CONSTRAINT "users_pk" PRIMARY KEY ("id")
 ) WITH (

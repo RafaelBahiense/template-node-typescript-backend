10350b64718ac7ccfdcbaed1dfcda6ca4e2b1d9a
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-07-26 02:02:43 -0300

    update server entry point

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import "./config/env";
 import app from "./app";
 
 const { NODE_ENV, PORT } = process.env;
-const dev: string = NODE_ENV === "development" ? " on Dev mode" : "";
+const dev = NODE_ENV === "development" ? " on Dev mode" : "";
 
-app.listen(PORT, () => {
+app.listen(parseInt(PORT || "4000"), () => {
   console.log(`Server runing on port ${PORT + dev}`);
 });

c70d6c14afbbc225c7d53ae935a42ba3c3599c99
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-08-10 13:50:50 -0300

    new login controller

--- src/controllers/auth/loginController.ts
+++ src/controllers/auth/loginController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
 import * as loginServices from "../../services/auth/loginServices";
+import { LoginUser } from "../../interfaces/userInterfaces";
 
 export default async function login(
   req: Request,
@@ -8,14 +9,11 @@ export default async function login(
   next: NextFunction
 ) {
   try {
-    const { email, password } = req.body;
+    const { email, password }: LoginUser = req.body;
     if (!(email && password)) return res.sendStatus(400);
 
-    const login = await loginServices.login(email, password);
-
-    if (login) res.status(200).send(login);
-    if (login === undefined) res.sendStatus(404);
-    else res.sendStatus(401);
+    const token = await loginServices.login({ email, password });
+    res.status(200).send({ token });
 
   } catch (e) {
     next(e);

4d2685547ce5ea896182e6c276323bf638673fa1
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-08-10 13:51:15 -0300

    new login services

--- src/services/auth/loginServices.ts
+++ src/services/auth/loginServices.ts
@@ -1,27 +1,24 @@
+import { getRepository } from "typeorm";
 import bcrypt from "bcrypt";
 
 import { Login } from "../../schemas/login";
-import repositories from "../../repositories/repositories";
-import * as auth from "../../utilities/authentication";
+import { LoginUser } from "../../interfaces/userInterfaces";
+import User from "../../entities/User";
+import Sessions from "../../entities/Sessions";
+import * as auth from "../../middlewares/authentication";
 import * as error from "../../types/errorTypes";
 
-export async function login(email: string, password: string) {
+export async function login({ email, password }: LoginUser) {
   await Login.validateAsync({ email, password });
 
-  const user = await repositories.user.getByEmail(email);
+  const user = await getRepository(User).findOne({ email });
   if (!user) throw error.service.notExistent();
-  const { hash, id: userId, name } = user;
 
+  const { hash, id: userId, name } = user;
   if (!bcrypt.compareSync(password, hash)) throw error.service.wrongPassword();
 
-  const sessions = await auth.checkExpired(await repositories.session.get(userId));
+  const token = auth.create(userId, email);
+  await getRepository(Sessions).insert({ userId, token });
 
-  if (sessions.length > 0) {
-    const { token } = sessions[0];
-    return { userId, name, token };
-  }
-
-  const token = auth.create(userId, name);
-  await repositories.session.register(userId, token);
   return { userId, name, token };
 }

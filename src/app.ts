3598d2076db575e7d115d15a9456d21d1d30b8eb
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-08-10 12:46:38 -0300

    app typeorm configs

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,21 @@
 import express, { Application } from "express";
 import cors from "cors";
+import "reflect-metadata";
 
 import { MainRouter } from "./routes/mainRouter";
-import errorHandler from "./controllers/error/errorController";
+import errorHandler, { jsonError } from "./controllers/error/errorController";
+import connectDatabase from "./config/database";
 
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(jsonError);
 app.use("/api", MainRouter);
 app.use(errorHandler);
+
+export async function init() {
+  await connectDatabase();
+}
 
 export default app;
